a3ad716f16d1e57e7b0a7b483cabe98f1936ff91
unknown <gwanjuchung@yahoo.com>
2022-08-25 15:12:27 -0700

    Created statement for comparing current hour to i

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 //console.log('hello!');
 // moment.js docs https://momentjs.com/
 var currentDayEl = document.getElementById("currentDay");
+var timeEntries = document.getElementById("timeEntries");
 
 // DECLARE Current time
 var currentTime = moment().format('LT');
@@ -27,9 +28,22 @@ for(var i = 9; i <= 17; i++){
     var data = "";
 
     // Compare i to current hour to determine if this hour is in the past, present, or future
+    //variable for past, present, or future
+    var time = '';
+
+    if(currentHour > i){
+      //if i is in the past
+      time = 'past';
+    } else if(currentHour < i) {
+      //if i is in the future
+      time = 'future';
+    } else{
+      //if i is in the present
+      time = 'present';
+    }
 
     var template = `
-    <div class="row">
+    <div class="row ${time}">
         <div>
           ${i}AM
         </div>
@@ -44,7 +58,23 @@ for(var i = 9; i <= 17; i++){
     `;
 
     // Append the html to page
-
+    // id = 'timeEntries'
+    var outputHTML = document.createElement('div');
+    outputHTML.innerHTML = `
+    <div class="row ${time}">
+        <div>
+          ${i}AM
+        </div>
+        <div>
+          <textarea>${data}</textarea>
+        </div>
+        <div>
+          <button class="saveBtn" data-hour="${i}">Save</button>
+        </div>
+      </div>
+    </div>
+    `;
+    timeEntries.appendChild(outputHTML);
 }
 
 // Set up a "click" event listener on the container
